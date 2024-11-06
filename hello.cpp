ea00929c0ff5248e4d0e620841edd73335a4ba07
Pranav Pramod <148346573+PranavPHe@users.noreply.github.com>
2024-11-05 19:07:24 -0500

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -33,29 +33,34 @@ int main()
     int supplies = 0;
     int day = 0;
 
-    // 
+    // Runs while there is energy and the supplies is not at 10
     while (energy > 0 && supplies < 10)
     {
         int choice;
         day++;
+        // Creates a day counter (Extra)
         cout << "\nDay " << day << ":" << endl;
 
+        // Asks the user to enter 1 or 2
         cout << "1. Search for supplies (costs 1 energy, adds 1 supply)\n2. Rest(restores 2 energy, but doesn’t add supplies)" << endl;
         cin >> choice;
 
         switch (choice)
         {
         case 1:
+            // If they enter 1, subtract energy, but add supplies. Print out the remaining. 
             energy--;
             supplies++;
             cout << "Energy: " << energy << endl;
             cout << "Supplies: " << supplies << endl;
             break;
         case 2:
+            // If they enter 2, add 2 energy.
             energy += 2;
             break;
         }
 
+        // If supplies exceeds or equals 10, You win. Otherwise, if you run out of energy before you reach 10 supplies, you will lose. Prints a message accordingly.
         if (supplies >= 10)
         {
             cout << "\nCongratulations" << endl;
@@ -67,6 +72,7 @@ int main()
     }
 
     // Exercise 3
+    // Creates a random 3 digit code. 
     random_device dev;
     mt19937 rng(dev());
     uniform_int_distribution<mt19937::result_type> dist6(100, 999);
@@ -76,16 +82,21 @@ int main()
 
     do
     {
+        // Takes in user input for a guess
         cout << "Enter the three digit passcode: " << endl;
         cin >> guess;
 
+        // If the guess is equal to the random code, you unlock the box. 
         if (guess == dist6(rng))
         {
             cout << "You unlocked the box!" << endl;
             break;
         }
+        // Otherwise, you will be asked to try again.
         cout << "Do you want to try again? (Y/N)" << endl;
         cin >> tryAgain;
+
+        // This loop will continue until the user enters a letter that is neither N nor n.
     } while (tryAgain != 'N' && tryAgain != 'n');
 
     return 0;
